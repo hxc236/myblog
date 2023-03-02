44987d88c7d34f5028907df1fa0f016b82ef5038
hxc <cei196505153@163.com>
2023-03-02 23:01:52 +0800

    创建前端并创建前端导航栏

---
+++ myblogweb/src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">我的博客</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#MyBlogNavbar"
+                aria-controls="MyBlogNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="MyBlogNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">新鲜事</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">聊天室</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">写写代码吧！</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            我的
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+  
+<script>
+
+</script>
+  
+<style scoped></style>
